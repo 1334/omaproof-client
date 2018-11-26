40a287cc99aad88473eb65a313caa3cf7d9c40ce
1334 <1334@shadowproject.net>
2018-11-26 14:23:30 +0100

    extract modal pose components to it's own file

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import posed, { PoseGroup } from 'react-pose';
+import { PoseGroup } from 'react-pose';
 
 import Posts from './posts';
 import NewPost from './newPost';
+import { Modal, ModalBackground } from './animations/modal';
 
 import data from '../data/oma.json';
-
-const Modal = posed.div({
-  enter: {
-    y: 0,
-    opacity: 1,
-    delay: 300,
-    transition: {
-      y: { type: 'spring', stiffness: 1000, damping: 15 },
-      default: { duration: 300 }
-    }
-  },
-  exit: {
-    y: 50,
-    opacity: 0,
-    transition: { duration: 150 }
-  }
-});
-
-const Shade = posed.div({
-  enter: { opacity: 1 },
-  exit: { opacity: 0 }
-});
 
 class Feed extends React.Component {
   state = {
@@ -46,7 +25,7 @@ class Feed extends React.Component {
         <button onClick={this.toggleNewPost}>Add new post</button>
         <PoseGroup>
           {this.state.newPost && [
-            <Shade
+            <ModalBackground
               key="shade"
               className="shade"
               onClick={this.toggleNewPost}
