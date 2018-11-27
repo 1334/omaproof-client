f10b163ee5c06225c0f2e428e30990cca1436bd7
1334 <1334@shadowproject.net>
2018-11-27 13:14:15 +0100

    style comments

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import CommentUser from './commentUser';
+
+const StyledComment = styled.div`
+  position: relative;
+  margin: 0.8em 0 0.8em 3em;
+
+  .user img {
+    position: absolute;
+    left: -3em;
+    border-radius: 50%;
+  }
+`;
 
 export default class Comment extends React.Component {
   render() {
     const { comment } = this.props;
     return (
-      <div className="comment">
-        {comment.content}
+      <StyledComment>
         <CommentUser user={comment.user} />
-        <hr />
-      </div>
+        {comment.content}
+      </StyledComment>
     );
   }
 }
