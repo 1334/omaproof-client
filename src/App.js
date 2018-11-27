d6a99100b1f337a9f751f3748263844647bd35df
1334 <1334@shadowproject.net>
2018-11-27 10:27:24 +0100

    extract navBar component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Feed from './components/feed';
 import Auth from './components/auth';
 import NewPost from './components/newPost';
+import NavBar from './components/navBar';
 
 const theme = {
   black: 'rgb(30,34,38)',
   grey: 'rgb(119,119,119)',
-  blue: 'rgb(14,52,82)',
+  navBarBgColor: 'rgb(14,52,82)',
   creme: 'rgb(246,244,234)'
 };
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Titillium+Web:200,300,400,700');
 
-  * {
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    line-height: 2;
     font-family: 'Titillium Web', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: white;
   }
 `;
 
@@ -33,19 +47,7 @@ class App extends Component {
           <GlobalStyle />
           <Router>
             <div>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/login">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="/new-post">New Post</Link>
-                  </li>
-                </ul>
-              </nav>
+              <NavBar />
               <Route path="/" exact component={Feed} key="home" />
               <Route path="/login" exact component={Auth} key="login" />
               <Route
