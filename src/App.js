25981ba8b445d4dcfd4e03b1e9415c72627780e4
1334 <1334@shadowproject.net>
2018-11-28 15:01:57 +0100

    fix merging conflicts errors

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-import './App.css';
-
 import Feed from './components/feed';
 import Auth from './components/auth';
 import NewPost from './components/newPost';
@@ -62,49 +60,26 @@ class App extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <GlobalStyle />
-          <Router>
-            <div>
-              <NavBar />
-=======
-      <ApolloProvider client={client}>
-        <div className="App">
-          <Router>
-            <div>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/login">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="/new-post">New Post</Link>
-                  </li>
-                </ul>
-              </nav>
->>>>>>> add apollo client to the stack
-              <Route path="/" exact component={Feed} key="home" />
-              <Route path="/login" exact component={Auth} key="login" />
-              <Route
-                path="/new-post"
-                exact
-                component={NewPost}
-                key="new-post"
-              />
-            </div>
-          </Router>
-<<<<<<< HEAD
-        </React.Fragment>
+        <ApolloProvider client={client}>
+          <React.Fragment>
+            <GlobalStyle />
+            <Router>
+              <div>
+                <NavBar />
+                <Route path="/" exact component={Feed} key="home" />
+                <Route path="/login" exact component={Auth} key="login" />
+                <Route
+                  path="/new-post"
+                  exact
+                  component={NewPost}
+                  key="new-post"
+                />
+              </div>
+            </Router>
+          </React.Fragment>
+        </ApolloProvider>
       </ThemeProvider>
-=======
-        </div>
-      </ApolloProvider>
->>>>>>> add apollo client to the stack
     );
   }
 }
