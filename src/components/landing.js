87ca83b28cde3ad3649a4b4864d72eb7627fe092
1334 <1334@shadowproject.net>
2018-12-06 00:45:16 +0100

    improve landing styling

--- src/components/landing.js
+++ src/components/landing.js
@@ -8,16 +8,15 @@ import { Mutation } from 'react-apollo';
 import GRAND_PARENT_LOGIN_MUTATION from '../graphql/mutations/grandParentLogin';
 import Logo from '../styledComponents/logo';
 
-const LandingBackground = styled.div`
+const StyledLanding = styled.div`
   background: url('https://res.cloudinary.com/truroer/image/upload/v1544016596/allen-taylor-709552-unsplash_copy.jpg')
     no-repeat center center fixed;
   background-size: cover;
-`;
 
-const StyledLanding = styled.div`
   display: flex;
   flex-direction: column;
   width: 100vw;
+  height: 100vh;
   margin: 0 auto;
   align-items: center;
 
@@ -57,88 +56,84 @@ const StyledLanding = styled.div`
 class Landing extends React.Component {
   render() {
     return (
-      <LandingBackground>
-        <StyledLanding>
-          <h3 className="lg">Welcome to </h3>
-          <div className="omaproof lg">
-            <Logo />
-          </div>
-          <div className="lg">a family friendly way to connect...</div>
+      <StyledLanding>
+        <h3 className="lg">Welcome to </h3>
+        <div className="omaproof lg">
+          <Logo />
+        </div>
+        <div className="lg">a family friendly way to connect...</div>
 
-          <div
-            style={{
-              marginTop: '5vh',
-              height: '40vh',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'space-between'
-            }}
-          >
-            <Link to="/create-group">
-              <Button
-                className="buttonLanding"
-                style={{
-                  width: '50vw',
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}
-              >
-                <div>Create new group</div>
-              </Button>
-            </Link>
-
-            <div className="lg">OR</div>
-            <Mutation
-              mutation={GRAND_PARENT_LOGIN_MUTATION}
-              variables={{
-                sessionToken: null,
-                selected: [],
-                unselected: []
+        <div
+          style={{
+            marginTop: '5vh',
+            height: '40vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Link to="/create-group">
+            <Button
+              className="buttonLanding"
+              style={{
+                width: '50vw',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
               }}
             >
-              {omaLogin => (
-                <Button
-                  type="submit"
-                  className="buttonLanding"
-                  style={{
-                    width: '80vw',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    padding: '4vw',
-                    fontSize: '6vw',
-                    marginBottom: '5vh'
-                  }}
-                  onClick={e => {
-                    e.preventDefault();
-                    omaLogin().then(({ data }) => {
-                      localStorage.setItem(
-                        'omalogin',
-                        JSON.stringify(data.grandParentLogin)
-                      );
-                      navigate('/oma-login');
-                    });
-                  }}
-                >
-                  <span style={{ marginRight: '3vw' }}>
-                    Oma, click to start{' '}
-                  </span>
-                  <span className="icon-arrow-right" />
-                </Button>
-              )}
-            </Mutation>
+              <div>Create new group</div>
+            </Button>
+          </Link>
 
-            <span>
-              <span className="lg1">You can also </span>
-              <Link to="/regular-login" className="links lg">
-                Log in with password
-              </Link>
-            </span>
-          </div>
-        </StyledLanding>
-      </LandingBackground>
+          <div className="lg">OR</div>
+          <Mutation
+            mutation={GRAND_PARENT_LOGIN_MUTATION}
+            variables={{
+              sessionToken: null,
+              selected: [],
+              unselected: []
+            }}
+          >
+            {omaLogin => (
+              <Button
+                type="submit"
+                className="buttonLanding"
+                style={{
+                  width: '80vw',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  padding: '4vw',
+                  fontSize: '6vw',
+                  marginBottom: '5vh'
+                }}
+                onClick={e => {
+                  e.preventDefault();
+                  omaLogin().then(({ data }) => {
+                    localStorage.setItem(
+                      'omalogin',
+                      JSON.stringify(data.grandParentLogin)
+                    );
+                    navigate('/oma-login');
+                  });
+                }}
+              >
+                <span style={{ marginRight: '3vw' }}>Oma, click to start </span>
+                <span className="icon-arrow-right" />
+              </Button>
+            )}
+          </Mutation>
+
+          <span>
+            <span className="lg1">You can also </span>
+            <Link to="/regular-login" className="links lg">
+              Log in with password
+            </Link>
+          </span>
+        </div>
+      </StyledLanding>
     );
   }
 }
